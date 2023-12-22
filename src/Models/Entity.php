c25a7cb94c5456b4a4c9dd29d70c32f8a31d8d24
DavAguayo <david@cuatromedios.com>
2023-12-22 01:39:38 -0600

    Base Entity Model & Migration Added

---
+++ src/Models/Entity.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace KusikusicmsModels\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use KusikusicmsModels\Models\Traits\UsesShortId;
+use KusikusicmsModels\Casts\Json;
+
+class Entity extends Model
+{
+    use UsesShortId, HasFactory, SoftDeletes;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'entities';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'id',
+        'model',
+        'view',
+        'properties',
+        'visibility',
+        'parent_entity_id',
+        'created_at',
+        'published_at',
+        'unpublished_at'
+    ];
+
+    protected $contentFields = [];
+
+    protected $propertiesFields = [];
+
+    protected $ancestorsRelations = true;
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'properties' => Json::class,
+        'published_at' => 'datetime',
+        'unpublished_at' => 'datetime',
+        'langs' => 'array'
+    ];
+}
